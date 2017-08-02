e04bdc55c5772b3e30221dfc3e5f522283c2ddbe
listoel <listoel@users.noreply.github.com>
2017-08-02 15:08:47 +0200

    New nominal plotter

--- plot_sliced.py
+++ plot_sliced.py
@@ -55,6 +55,7 @@ if __name__ == "__main__":
         datproc.lossplot(lossfolder, xlim=[0.0679, 0.0685], ylim=[-0.001465, -0.00145], cax="PT", save=plotfolder+"/beam_start_zoom.png")
         datproc.lossplot(lossfolder, xlim=[0.079, 0.083], ylim=[-0.00175, -0.0017], cax="PT", save=plotfolder+"/beam_end.png")
         datproc.lossplot(lossfolder, xlim=[0.0795, 0.0815], ylim=[-0.00176, -0.0017], cax="PT", save=plotfolder+"/beam_end_zoom.png")
+        datproc.losshistscatter(lossfolder, lossloc=lossloc, cax="PT", xlim=[0.06815,0.08315], ylim=[-0.0018, -0.00135], xbin=0.0002, ybin=0.00001, log=False, save=plotfolder+"/losshist.png")
     else:
         datproc.efficiency(lossfolder, pycoll=True, aperturex=[0.04219,0.08219], aperturey=[-0.0100,0.0100])
         print ""
@@ -62,14 +63,13 @@ if __name__ == "__main__":
 
         #datproc.zsbacktrack(trackfolder, cax="TURN", save_d=plotfolder+"/zs_down_backtrack_"+name+".png", save_u=plotfolder+"/zs_up_backtrack_"+name+".png")
 
-        datproc.losshistscatter(lossfolder, lossloc=lossloc, xlim=[0.03759, 0.13], ylim=[0,0.004], xbin=0.001, ybin=0.00005, log=True, save=plotfolder+"/tpst_losshist_"+name+".png")
+        datproc.losshistscatter(lossfolder, lossloc=lossloc, xlim=[0.03759, 0.13], ylim=[0,0.004], xbin=0.001, ybin=0.00005, log=True, save=plotfolder+"/tpst_losshist_h.png")
         datproc.losshistscatter(lossfolder, lossloc=lossloc, xax='Y', yax='PY', xlim=[-0.04, 0.04], ylim=[-0.002,0.002], xbin=0.001, ybin=0.00005, log=True, save=plotfolder+"/tpst_losshist_v_.png")
         datproc.losshistscatter(lossfolder, lossloc=lossloc, xax='X', yax='Y', xlim=[0.03759, 0.13], ylim=[-0.04, 0.04], xbin=0.001, ybin=0.001, log=True, save=plotfolder+"/tpst_losshist_s.png")
 
-        datproc.lossplot(lossfolder, lossloc=lossloc, xax='TURN', yax='PT', cax='X', save=plotfolder+"/sweep.png")
         datproc.lossplot(lossfolder, lossloc=None, xlim=[1668,1688], xax='S', yax='X', cax='PT', clim=(-0.0015,0.0015), save=plotfolder+"/zs_loss.png")
 
 
-    datproc.losshistscatter(lossfolder, lossloc=lossloc, cax="PT", xlim=[0.06815,0.08315], ylim=[-0.0018, -0.00135], xbin=0.0002, ybin=0.00001, log=False, save=plotfolder+"/losshist.png")
+    datproc.lossplot(lossfolder, lossloc=lossloc, xax='TURN', yax='PT', cax='X', save=plotfolder+"/sweep.png")
     datproc.trackplot(trackfolder, obsloc="obs0002", cax="PT", tpt=300, save=plotfolder+"/zs_up.png")
     datproc.trackplot(trackfolder, obsloc="obs0003", cax="PT", tpt=300, save=plotfolder+"/zs_down.png")
